af140e14b1ee6a7da9322709108ae708541806d6
Brendan Woo <woo.brendan@gmail.com>
2023-05-22 10:55:05 -0700

    in class Category, add meta class for ordering and naming of plurality. change string to be returned as self.name. Create item class with various columns

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='items_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
