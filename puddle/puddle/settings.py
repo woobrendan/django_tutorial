597e9a799f7770cbc50fceaeab54b469373b57ed
Brendan Woo <woo.brendan@gmail.com>
2023-05-26 13:34:31 -0700

    update file to use dotenv for secret key

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%=vll90x8b3_!ww9zg*rokvkqs7s+o)gn$op1kvyh$%i00*5b3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
