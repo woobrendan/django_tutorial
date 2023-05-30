5f44178b65164a5990ea5e86d88a4559579036a8
Brendan Woo <woo.brendan@gmail.com>
2023-05-30 11:02:04 -0700

    fix misspelled Category import

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from item.models import Catageory, Item
+from item.models import Category, Item
 
 def index(request):
   items = Item.objects.filter(is_sold=False)[0:6]
