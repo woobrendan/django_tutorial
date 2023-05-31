e59ba4c26e3d4e28c7fc6b3a03a8783e836b3683
Brendan Woo <woo.brendan@gmail.com>
2023-05-31 11:17:43 -0700

    comment out imports for index and contact. fix heom path and remove index view passed to path

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -19,12 +19,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import index, contact
+# from core.views import index, contact
 
 #include works like middleware and uses the passed in file
 
 urlpatterns = [
-    path('', index, include('core.urls')),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
