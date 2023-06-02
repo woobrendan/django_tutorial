2345176d597bfc21b41bc2b0402b3d01c960e123
Brendan Woo <woo.brendan@gmail.com>
2023-06-02 12:06:09 -0700

    create redirect options for Login and logout

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -117,3 +117,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication Redirection
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
