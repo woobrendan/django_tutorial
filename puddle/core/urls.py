fd652aa036db0194d3502e1d0071537c7621db75
Brendan Woo <woo.brendan@gmail.com>
2023-06-02 11:54:37 -0700

    import auth view, create new route login, passing along django view as name login

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +8,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name="contact"),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login')
 ]
