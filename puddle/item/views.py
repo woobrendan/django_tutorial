486710eb36b574014ae1af528823d0c75a99c348
Brendan Woo <woo.brendan@gmail.com>
2023-06-05 11:52:35 -0700

    add in login_required authentication for decorator. create new controller for new, passing newItemForm and rendering form and title

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
 from .models import Item
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
@@ -8,4 +11,13 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
+    })
+
+@login_required
+def new(request):
+    form = NewItemForm()
+
+    return render(request, 'item/form/html', {
+        'form': form,
+        'title': 'New Item'
     })
