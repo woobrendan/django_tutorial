95f67c2cbc1da46623887a81c58d68c08fbb501a
Brendan Woo <woo.brendan@gmail.com>
2023-06-07 11:20:53 -0700

    update edit controller. get item for edit using pk. add instance to form variables. remove is_valid created_by and just save form

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -43,18 +43,18 @@ def delete(request, pk):
 
 @login_required
 def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, create_by=request.user)
+
     if request.method == 'POST':
-        form = EditItemForm(request.POST, request.FILES)
+        form = EditItemForm(request.POST, request.FILES, instance=item)
 
         if form.is_valid():
-            item = form.save(commit=False)
-            item.created_by = request.user
-            item.save()
+            form.save()
 
             return redirect('item:detail', pk=item.id)
     
     else:
-        form = EditItemForm()
+        form = EditItemForm(instance=item)
 
     return render(request, 'item/form.html', {
         'form': form,
