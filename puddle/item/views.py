686450f33af22ecdae7d95115d2bd92c01378a5f
Brendan Woo <woo.brendan@gmail.com>
2023-06-08 12:59:26 -0700

    create if block for cat id and getting only items that match the cat

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -11,6 +11,9 @@ def items(request):
     category_id = request.GET.get('category', 0)
     items = Item.objects.filter(is_sold=False)
 
+    if category_id:
+        items = items.filter(category_id=category_id)
+
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
