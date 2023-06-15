def441618b35e354946c57df22bb358c4c25e530
Brendan Woo <woo.brendan@gmail.com>
2023-06-15 11:44:51 -0400

    add sort to items that will order items by recency. add dollar sign to price

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -4,16 +4,19 @@ from django.contrib.auth import logout
 from item.models import Category, Item
 from .forms import SignupForm
 
+
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6]
+    items = Item.objects.filter(is_sold=False).order_by('-created_by')[0:6]
     categories = Category.objects.all()
     return render(request, 'core/index.html', {
         'categories': categories,
         'items': items
     })
 
+
 def contact(request):
     return render(request, 'core/contact.html')
+
 
 def signup(request):
     # if method is post, validate the form and save. else return signup form
@@ -29,6 +32,7 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form': form
     })
+
 
 def logout_view(request):
     logout(request)
