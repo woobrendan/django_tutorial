40d40276378940e0772b0028f08b2450cf02d2e0
Brendan Woo <woo.brendan@gmail.com>
2023-06-15 17:05:00 -0700

    add in car_parent variable to category, to allow for nested categories of cars

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    car_parent = models.ForeignKey(
+        'self', null=True, blank=True, related_name="car_child", on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('name',)
@@ -11,15 +14,18 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Item(models.Model):
-    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
     image = models.ImageField(upload_to='items_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, related_name='items', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
